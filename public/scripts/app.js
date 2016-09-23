79413da913be5824658e498484283497bd836e90
Sarah Veness <sarahhveness@gmail.com>
2016-09-23 23:57:25 +0000

    general cleanup, toggle compose button view on, counter and tweet button disabled if tweet too long

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
 
   updateCountdown();
-    // $('.text').paste(updateCountdown);
     $('.text').on("keyup change", (updateCountdown));
 
+  loadTweets();
 
-loadTweets();
-// renderTweets(data);
+  $('#hideshow').on('click', function(event) {
+    $('#compose-tweet').toggle('show');
+    $('#textarea').focus();
+  });
 
-// $.getJSON("/tweets", function(json) {
-// }
 
-  // function renderTweets(tweets) {
-  //   for each (var user in tweetData) {
-  //     createTweetElement()();
-  //     $('.tweet').prepend(newTweet);
-  //   }
-  // }
+
   function loadTweets() {
     $.ajax({
       url: '/tweets',
@@ -24,58 +19,49 @@ loadTweets();
       dataType: 'json',
       success: function(result) {
         console.log('Success, you"re so smart!');
-        $('#tweetcontainer').append(renderTweets(result));
+        renderTweets(result);
       }
     });
-  // });
   }
 
-   function renderTweets(tweets) {
-  // loops through tweets
-  tweets.forEach(function(tweet) {
-    $('#tweetcontainer').append(createTweetElement(tweet));
-    // return tweet;
-  });
-}
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweet) {
+      $('#tweetcontainer').append(createTweetElement(tweet));
+    });
+  }
 
-function createTweetElement(tweet) {
-  var articleTweet = $('<article>').addClass('tweet');
-  articleTweet.append($(`
-    <header>
-      <img src="${tweet.user.avatars.regular}"/>
-      <p class='handle'>${tweet.user.handle}</p>
-      <p class='username'>${tweet.user.name}</p>
-    </header>
-    <span class='tweet-content'>${tweet.content.text}</span>
-    <footer>
-      <p class='date'>${tweet.created_at}</p>
-      <div class="buttons-right">
-      <img src="/images/flag.png"/>
-      <img src="/images/retweet.png"/>
-      <img src="/images/heart.png"/>
-      </div>
-    </footer>`
-  ));
-  return articleTweet;
-};
-
-
-
+  function createTweetElement(tweet) {
+    var articleTweet = $('<article>').addClass('tweet');
+    articleTweet.append($(`
+      <header>
+        <img src="${tweet.user.avatars.regular}"/>
+        <p class='handle'>${tweet.user.handle}</p>
+        <p class='username'>${tweet.user.name}</p>
+      </header>
+      <span class='tweet-content'>${tweet.content.text}</span>
+      <footer>
+        <p class='date'>${tweet.created_at}</p>
+        <div class="buttons-right">
+        <img src="/images/icon-flag.png"/>
+        <img src="/images/icon-retweet.png"/>
+        <img src="/images/icon-like.png"/>
+        </div>
+      </footer>`
+    ));
+    return articleTweet;
+  };
 
   $("form").on("submit", function newTweet(event) {
     event.preventDefault();
-    var tweet = $(this)  //'this' being your form info
+    var tweet = $(this).serialize();  //'this' being your form info
     $.ajax( {
-    url: '/tweets',
-    method: 'POST',
-    data: tweet.serialize(),
-    success: function() {
-      console.log("Success, you're posting a new tweet!");
-      // $('#tweetcontainer').prepend(renderTweets(tweet));
-
-    }
+      url: '/tweets',
+      method: 'POST',
+      data: tweet,
+      dataType: 'json',
+      success: function(tweet) {
+         $('#tweetcontainer').prepend(createTweetElement(tweet));
+      }
     });
   });
-
-
 });
